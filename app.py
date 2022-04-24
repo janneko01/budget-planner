1d76c1774e74682c48319617958117100a2e9a38
Janne Koivurova <48507101+janneko01@users.noreply.github.com>
2022-04-24 22:15:38 +0300

    Database update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
